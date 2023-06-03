ada3dcb664b68391491f0403a4e201cd4f82af42
Guillaume Bouvignies <gbouvignies@gmail.com>
2023-06-03 09:10:11 +0200

    Update with questions and binding mechanism

--- simulator.py
+++ simulator.py
@@ -1,79 +1,87 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import numpy as np
 from scipy.linalg import eig
 
 GAMMA_N15 = -2.712_618_04e07
 
 
-def compute_liouvillian(
-    b0: float,
-    pb: float,
-    kex: float,
-    δa: float,
-    δb: float,
-    r2a: float,
-    r2b: float,
-) -> np.ndarray:
-    """
-    Compute the Liouvillian matrix for the 1D exchange spectrum.
-    """
-    pa = 1.0 - pb
-    kab = kex * pb
-    kba = kex * pa
+@dataclass
+class Parameters:
+    b0: float = 18.7
+    pb: float = 0.2
+    kex: float = 200.0
+    δa: float = -2.0
+    δb: float = 2.0
+    r2a: float = 10.0
+    r2b: float = 10.0
 
-    wa_rads = GAMMA_N15 * b0 * δa * 1e-6
-    wb_rads = GAMMA_N15 * b0 * δb * 1e-6
 
-    liouvillian = np.zeros((2, 2), dtype=complex)
-    liouvillian[0, 0] = -r2a - kab + 1j * wa_rads
-    liouvillian[0, 1] = kba
-    liouvillian[1, 0] = kab
-    liouvillian[1, 1] = -r2b - kba + 1j * wb_rads
+@dataclass
+class Component:
+    r2: float
+    frq: float
+    intensity: float
+    phase: float
+
+
+@dataclass
+class Coordinates:
+    x: np.ndarray
+    y: np.ndarray
+    y1: np.ndarray
+    y2: np.ndarray
+
+
+def lorentzian(x: np.ndarray, comp: Component) -> np.ndarray:
+    a = comp.r2**2 + (comp.frq - x) ** 2
+    return comp.intensity * (
+        np.cos(comp.phase) * comp.r2 / a + np.sin(comp.phase) * (comp.frq - x) / a
+    )
+
+
+def compute_liouvillian(params: Parameters) -> np.ndarray:
+    pa = 1.0 - params.pb
+    kab = params.kex * params.pb
+    kba = params.kex * pa
+
+    ωa = GAMMA_N15 * params.b0 * params.δa * 1e-6
+    ωb = GAMMA_N15 * params.b0 * params.δb * 1e-6
+
+    liouvillian = 1j * np.diag([ωa, ωb])
+    liouvillian += np.array([[-kab, +kba], [+kab, -kba]])
+    liouvillian += np.diag([-params.r2a, -params.r2b])
 
     return liouvillian
 
 
-def get_spectral_components(
-    liouvillian: np.ndarray, pb: float
-) -> list[tuple[float, float, float, float]]:
-    """
-    Compute the spectral components from the Liouvillian matrix.
-    """
+def get_spectral_components(params: Parameters) -> list[Component]:
+    liouvillian = compute_liouvillian(params)
+
     eigen_values, eigen_vectors = eig(liouvillian)
-    r2_values = -1.0 * eigen_values.real
-    w_values = eigen_values.imag
 
-    pa = 1.0 - pb
-    m0 = np.array([[pa], [pb]])
-    eigen_vectors_inv = np.linalg.inv(eigen_vectors)
-    norm_vec = eigen_vectors_inv @ m0
+    r2s = -1.0 * eigen_values.real
+    frequencies = eigen_values.imag
 
-    # "tmp" is a pretty bad name...
-    # We detect Ma + Mb
+    norm_vec = np.linalg.inv(eigen_vectors) @ np.array([[1.0 - params.pb], [params.pb]])
     tmp = eigen_vectors.sum(0) * norm_vec.T
-    intensity_values = np.abs(tmp[0, :])
-    phase_values = -np.angle(tmp[0, :])
+    intensities = np.abs(tmp[0, :])
+    phases = -np.angle(tmp[0, :])
 
-    return list(zip(r2_values, w_values, intensity_values, phase_values))
+    components = [
+        Component(*values) for values in zip(r2s, frequencies, intensities, phases)
+    ]
 
-
-def lorentzian(
-    x: np.ndarray, r2: float, frq: float, intensity: float, phase: float
-) -> np.ndarray:
-    a = r2**2 + (frq - x) ** 2
-    return intensity * (np.cos(phase) * r2 / a + np.sin(phase) * (frq - x) / a)
+    return components
 
 
-def calculate_spectrum(
-    spectral_width: float, b0: float, spectral_components
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """
-    Calculate the 1D exchange spectrum.
-    """
-    x = 0.5 * spectral_width * np.linspace(-1.0, 1.0, 5000)
-    x_rads = GAMMA_N15 * b0 * x * 1e-6
-    y1 = lorentzian(x_rads, *spectral_components[0])
-    y2 = lorentzian(x_rads, *spectral_components[1])
+def calculate_spectrum(spectral_width: float, params: Parameters) -> Coordinates:
+    x = spectral_width * np.linspace(-0.5, 0.5, 5000)
+    x_rads = x * GAMMA_N15 * params.b0 * 1e-6
+    components = get_spectral_components(params)
+    y1 = lorentzian(x_rads, components[0])
+    y2 = lorentzian(x_rads, components[1])
     y = np.asarray(sum((y1, y2)))
-    return x, y, y1, y2
+    return Coordinates(x, y, y1, y2)
